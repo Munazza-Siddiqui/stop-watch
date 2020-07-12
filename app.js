7de188b22b5872dd2aae9d37a968ff7c18adfce1
Munazza-Siddiqui <munazzasiddiqui22@gmail.com>
2020-07-12 13:11:32 +0500

    ready

---
+++ app.js
@@ -0,0 +1,39 @@
+var min=0;
+var sec=0;
+var msec=0;
+var minute=document.getElementById("min");
+var second=document.getElementById("sec");
+var milliSecond=document.getElementById("msec");
+var interval;
+
+function timer(){
+    msec++;
+    milliSecond.innerHTML=msec+' millisec';
+    if(msec == 100){
+        sec++;
+        second.innerHTML=sec+' sec &nbsp :';
+        msec=0;
+    }
+    if(sec ==60){
+        min ++;
+        minute.innerHTML=min+' min &nbsp :';
+        sec=0;
+    }
+}
+function start(){
+interval = setInterval(timer,10);
+}
+ 
+function stop(){
+
+    clearInterval(interval);
+}
+function reset(){
+    min=0;
+    sec=0;
+    msec=0;
+    minute.innerHTML=min+' min &nbsp :';
+    second.innerHTML= sec+' sec &nbsp :';
+    milliSecond.innerHTML= msec+' millisec';
+    clearInterval(interval);
+}
